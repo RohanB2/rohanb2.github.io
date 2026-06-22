4dff45a504ee727194d795b19808399dc59b9c69
RohanB2 <102451995+RohanB2@users.noreply.github.com>
2026-06-22 19:16:55 -0400

    Updated deployment file for Github pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Moon, Sun } from 'lucide-react'
 import { motion, AnimatePresence } from 'framer-motion'
-import './App.css'
 
 import Experience from './components/Experience'
 import Projects from './components/Projects'
